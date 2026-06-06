f294d006a94eb85362c7e918bfd077726973eaf7
Rithigaa Shree <133551846+rithudhoni@users.noreply.github.com>
2026-06-06 11:50:08 +0530

    Add files via upload

---
+++ Models_Python/ESN MODEL/esn_weights.vh
@@ -0,0 +1,7 @@
+// ESN Weight Parameters
+`define N_RESERVOIR 1024
+`define N_INPUT 3
+`define N_OUTPUT 3
+`define W_RES_NNZ 52146
+`define Q_SCALE 65536
+`define Q_SHIFT 16
---
+++ Models_Python/ESN MODEL/parameters.v
@@ -0,0 +1,10 @@
+// ESN Parameters
+parameter N_RESERVOIR = 1024;
+parameter N_INPUT = 3;
+parameter N_OUTPUT = 3;
+parameter Q_SHIFT = 16;
+parameter Q_SCALE = 65536;
+parameter LEAK_RATE = 19660; // Q16.16
+parameter N_TAPPED = 256;
+parameter TAP_INTERVAL = 4;
+parameter STM_P = 1717986918; // Q0.32
